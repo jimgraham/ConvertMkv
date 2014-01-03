bdf0ec2f1aded2ab413338561cfa80def796aba6
Jim Graham <jim@jim-graham.net>
2014-01-02 21:04:27 -0500

    Initial outline of perl script
    The initial outline of the perl script

---
+++ ConvertMkv.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Find;
+
+#-- find ffprobe and ffmpeg
+
+#-- get starting directory
+
+#-- get list of all .mkv files.
+
+#-- start processing files
+
+#-- loop
+
+#-- get best guess at name
+
+#-- look up in TVDB database
+#--  use your own API key for this.
+
+#-- encode the mkv videdo
+ 
