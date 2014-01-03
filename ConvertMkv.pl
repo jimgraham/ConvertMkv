cc038d3191cf855f3730242b9bbeebc2874e91ad
Jim Graham <jim@jim-graham.net>
2014-01-02 21:57:00 -0500

    Flesh out some more details
    Add
     - finding executables
     - `GetApiKey`
     - `ParseShowDetailsFromFileName`

--- ConvertMkv.pl
+++ ConvertMkv.pl
@@ -1,22 +1,61 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use File::Find;
+use File::Find::Rule;
+use File::Which;
+use Getopt::Long;
 
 #-- find ffprobe and ffmpeg
+my $ffmpeg_exe  = which('ffmpeg')  || die "Cannot find 'ffmpeg' in PATH";
+my $ffprobe_exe = which('ffprobe') || die "Cannot find 'ffprobe' in PATH";
 
-#-- get starting directory
+#-- get options (starting directory)
+
+#-- get API key for TVDB
+my $api_key = &GetApiKey();
 
 #-- get list of all .mkv files.
+my @mkv_files = File::Find::Rule->file()->name( '*.mkv' )->in( qw{ . } );
 
 #-- start processing files
+foreach my $mkv_file (@mkv_files)
+{
+	print STDOUT $mkv_file, "\n";
+	
+	#-- get best guess at name
+	my ($show_name, $season, $episode ) = &ParseShowDetailsFromFileName($mkv_file);
 
-#-- loop
+	#-- look up in TVDB database
+	#--  use your own API key for this. Read from API file
 
-#-- get best guess at name
 
-#-- look up in TVDB database
-#--  use your own API key for this.
+	#-- encode the mkv videdo
+}
 
-#-- encode the mkv videdo
- 
+=head2 GetApiKey
+
+Gets the API key from a file called "API" in the same directory as this file.
+
+=cut
+sub GetApiKey
+{
+	my $api_key = '';
+	if ( -e "API")
+	{
+		open my $file, '<', "API"; 
+		$api_key = <$file>; 
+		close $file;
+	}
+	$api_key;
+}
+
+=head2 ParseShowDetailsFromFileName
+
+Gets the best guess for a show name, season and episode number.
+
+=cut
+sub ParseShowDetailsFromFileName
+{
+	my $file_name = shift @_;
+	#-- return the show name, season, episode
+}
